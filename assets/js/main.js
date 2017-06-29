3858c7b9ab21ac4fa1808dc9d2a5bb7144a299a2
karachiani <karachiani@users.noreply.github.com>
2017-06-28 19:36:41 -0700

    sdfs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+
+function hideNavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+// function changeIcon(x) {
+//     x.classList.toggle("change");
+// }
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
